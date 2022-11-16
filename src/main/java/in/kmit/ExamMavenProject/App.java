f76c04976cb7896814e7939964727e999992c875
Eashwarsai,boini <20bd1a056b@gmail.com>
2022-11-16 11:54:19 +0530

    this is my project

---
+++ src/main/java/in/kmit/ExamMavenProject/App.java
@@ -0,0 +1,13 @@
+package in.kmit.ExamMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is SE lab internal 1 exam, the created project is ExamMavenProject." );
+    }
+}
